adbff680c1bea14e7cbba35fcfa80c971a773427
Rishi9822 <rishipatel9822@gmail.com>
2025-01-12 23:05:33 +0530

    12-1-25

--- js/main.js
+++ js/main.js
@@ -17,3 +17,20 @@ function opentab(tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+// Google sheet 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxBudlL0oggYBoVAL_TvC44jESBS6l89ZFKw2HqRJG8PNCLZg7bqiw5x5B8kq0AXD0W/exec'
+  const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent succesfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
